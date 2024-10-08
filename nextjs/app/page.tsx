0a2b3a6905615869538cdccc4c96ffaf3c00d6ff
matiassantander <matias.s@adipa.cl>
2024-10-08 15:38:51 -0300

    first commit

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white">Hello World</div>
+      <button className="bg-blue-500 text-white">Click me</button>
+    </div>
+  );
+}
